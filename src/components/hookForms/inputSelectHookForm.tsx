9ffc2dd65e6ada155508a3a5b0d5a32aeb409fd7
Edwin Vargas Ayala <88119497+Edwin3002@users.noreply.github.com>
2024-06-14 23:37:57 -0500

    Add files via upload

--- src/components/hookForms/inputSelectHookForm.tsx
+++ src/components/hookForms/inputSelectHookForm.tsx
@@ -25,7 +25,7 @@ export const InputSelectHookForm = ({ ...props }): JSX.Element => {
         defaultValue={getValues()[props.name]}
       >
         <SelectTrigger className="w-[280px]">
-          <SelectValue placeholder={`Seleciona la edad`} />
+          <SelectValue placeholder={`Seleciona una opción`} />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
